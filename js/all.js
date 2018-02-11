a4f7183df5bd53c6c6642f98a2e3830c8bdfe124
shihjian <31888231+shihjian@users.noreply.github.com>
2018-02-11 17:23:51 +0800

    Add files via upload

---
+++ js/all.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+
+document.querySelector('article').style.height = window.innerHeight+"px"
+
+window.onresize = function(){
+   document.querySelector('article').style.height = window.innerHeight+"px"
+}
+
+
+
+
+});
+
